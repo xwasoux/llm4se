ff0c2f94e1624d95dce769177441475a02392f68
xwasoux <oxwasouxo@gmail.com>
2023-07-02 22:08:13 +0000

    move & rename file name

--- SentenceCodeBERT/sh/preprocess.sh
+++ SentenceCodeBERT/sh/preprocess.sh
@@ -1,8 +1,8 @@
-mkdir data data/semantic_east_sim
-cd data/semantic_east_sim
+mkdir ../data ../data/semantic_east_sim
+cd ../data/semantic_east_sim
 gdown https://drive.google.com/uc?id=1n6OyDsNifUY0h9h8yNpWzvdkseqSly-9
 unzip semantic_east_sim_data.zip
 rm  semantic_east_sim_data.zip
 cd ../../semantic_east_sim
-python process_for_ses.py
+python ast_diff_preprocess.py
 cd ..
