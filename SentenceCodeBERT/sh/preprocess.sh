c324087388d23fc0c57de8680b90c9dcc37382b9
xwasoux <oxwasouxo@gmail.com>
2023-07-05 06:12:36 +0000

    delete

--- SentenceCodeBERT/sh/preprocess.sh
+++
@@ -1,8 +0,0 @@
-mkdir ../data ../data/semantic_east_sim
-cd ../data/semantic_east_sim
-gdown https://drive.google.com/uc?id=1n6OyDsNifUY0h9h8yNpWzvdkseqSly-9
-unzip semantic_east_sim_data.zip
-rm  semantic_east_sim_data.zip
-cd ../../semantic_east_sim
-python ast_diff_preprocess.py
-cd ..
