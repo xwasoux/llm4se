fd9cba08fb26c0380f4057099396f9eead3aff93
xwasoux <oxwasouxo@@gmail.com>
2023-08-21 15:41:14 +0000

    add: init src templates for fine-tuning

---
+++ CodeBERT/create.sh
@@ -0,0 +1,15 @@
+dir_name=ProjectName
+
+mkdir $dir_name
+cd $dir_name
+main_dir=$(pwd)
+
+mkdir data data/inputs 
+mkdir output
+mkdir sh
+
+cd sh
+touch preprocess.sh finetuning.sh inspect.sh
+
+cd $main_dir
+touch run.py preprocess.py inspect.py
