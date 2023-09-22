d1ed82eb0420e31f6510ff974639056246e60cdb
masa0702 <gp20a163@oecu.jp>
2023-09-22 06:49:41 +0000

    chore: remove unnecessary codes (legacy exp codes)

--- SentenceCodeBERT/diff-metric/sh/insp_diff_metric.sh
+++
@@ -1,3 +0,0 @@
-python3 ../diff_metric_inspect.py \
-    --model_path ../output/codebert_diff_microsoft-codebert-base-2023-07-02_22-20-01 \
-    --datasets /workspace/SentenceCodeBERT/data/semantic_diff_sim/dezero_diff_dataset.jsonl 
