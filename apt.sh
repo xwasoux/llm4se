aedc87b3aacb8efe0105b08e2a2f43854c6fc49e
xwasoux <oxwasouxo@gmail.com>
2024-01-16 12:26:34 +0000

    feat: add git config command

--- apt.sh
+++ apt.sh
@@ -7,3 +7,10 @@ sudo apt -y install \
     tmux \
     vim \
     tree
+
+echo "Enter your git account name: "
+read user
+echo "Enter your git account email: "
+read email
+git config --global user.name $user
+git config --global user.email $email
