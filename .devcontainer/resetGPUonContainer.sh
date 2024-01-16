9083d0045004d4f13c7e7a93f1ceaa4caa164ea1
xwasoux <oxwasouxo@gmail.com>
2024-01-16 11:27:47 +0000

    style: rename & format it

--- .devcontainer/resetGPUonContainer.sh
+++ .devcontainer/resetGPUonContainer.sh
@@ -4,10 +4,7 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
 
 curl -s -L https://nvidia.github.io/nvidia-container-runtime/experimental/$distribution/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
 
-sudo apt-get update
-
+sudo apt-get -y update
 sudo apt-get install -y nvidia-docker2
-
 sudo systemctl restart docker
-
 sudo docker run --rm --gpus all nvidia/cuda:11.0-base nvidia-smi
